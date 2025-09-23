05c930e3518de599f615b9614d2f91c96b38acaf
Manjunarayanaravuri <manju@korecent.com>
2025-09-23 17:48:56 +0530

    add new Doctypes to LCM app

---
+++ lcm/lcm/doctype/cad/test_cad.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Manju narayana ravuri and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCAD(FrappeTestCase):
+	pass
---
+++ lcm/lcm/doctype/engineering_change_order/engineering_change_order.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Manju narayana ravuri and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Engineering Change Order", {
+// 	refresh(frm) {
+
+// 	},
+// });
