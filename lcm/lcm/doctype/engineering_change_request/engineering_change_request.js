afdc0e91d6730211715fe466d29fbea0dc6d47bd
Manjunarayanaravuri <manju@korecent.com>
2025-09-25 10:42:02 +0530

    Add create eco button

--- lcm/lcm/doctype/engineering_change_request/engineering_change_request.js
+++ lcm/lcm/doctype/engineering_change_request/engineering_change_request.js
@@ -1,7 +1,35 @@
 // Copyright (c) 2025, Manju narayana ravuri and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Affected Items', {  
+// frappe.ui.form.on('Affected Items', {  
+//     item: function(frm, cdt, cdn) {
+//         let row = locals[cdt][cdn];
+
+//         if (row.item) {
+//             frappe.call({
+//                 method: "frappe.client.get_list",
+//                 args: {
+//                     doctype: "BOM",
+//                     filters: { item: row.item, is_active: 1 },
+//                     fields: ["name"],
+//                     limit_page_length: 1
+//                 },
+//                 callback: function(r) {
+//                     if (r.message && r.message.length > 0) {
+//                         row.bom = r.message[0].name;  
+//                     } else {
+//                         row.bom = "";
+//                     }
+//                     frm.refresh_field("affected_items"); 
+//                 }
+//             });
+//         }
+//     }
+// });
+
+
+
+frappe.ui.form.on('Affected Items', {
     item: function(frm, cdt, cdn) {
         let row = locals[cdt][cdn];
 
@@ -10,17 +38,21 @@ frappe.ui.form.on('Affected Items', {
                 method: "frappe.client.get_list",
                 args: {
                     doctype: "BOM",
-                    filters: { item: row.item, is_active: 1 },
+                    filters: { 
+                        item: row.item, 
+                        is_active: 1,
+                        is_default: 1  
+                    },
                     fields: ["name"],
                     limit_page_length: 1
                 },
                 callback: function(r) {
                     if (r.message && r.message.length > 0) {
-                        row.bom = r.message[0].name;  
+                        row.bom = r.message[0].name;
                     } else {
                         row.bom = "";
                     }
-                    frm.refresh_field("affected_items"); 
+                    frm.refresh_field("affected_items");
                 }
             });
         }
@@ -33,15 +65,22 @@ frappe.ui.form.on('Affected Items', {
 frappe.ui.form.on('Engineering Change Request', {
     refresh: function(frm) {
         
-        if (!frm.is_new()) {
-            frm.add_custom_button(__('Create ECO'), function() {
+        frm.clear_custom_buttons();
+
+        if (frm.doc.workflow_state === "Approved") {
+            frm.add_custom_button(__('Engineering Change Order'), function() {
                 frappe.new_doc('Engineering Change Order', {
-                    ecr_link: frm.doc.name  
+                    ecr_link: frm.doc.name
                 });
-            });
+            }, __("Create"));
         }
     }
 });
+
+
+
+
+
 
 
 
